056f263ff78af996b5c90e94425f955a7961a146
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-03-01 00:21:40 +0100

    Fix deprecation warnings

--- mysite/urls.py
+++ mysite/urls.py
@@ -10,7 +10,7 @@ admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', django.contrib.auth.views.login),
-    url(r'^accounts/logout/$', django.contrib.auth.views.logout, {'next_page': '/'}),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
